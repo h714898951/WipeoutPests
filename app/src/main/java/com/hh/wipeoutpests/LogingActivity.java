1de1c3ce7f2901b5112077f1384516985d7a53cb
h714898951 <714898951@qq.com>
2018-02-09 15:22:23 +0800

    initial commit

---
+++ app/src/main/java/com/hh/wipeoutpests/LogingActivity.java
@@ -0,0 +1,13 @@
+package com.hh.wipeoutpests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loging);
+    }
+}
